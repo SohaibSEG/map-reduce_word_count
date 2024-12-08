be7dfdbd059284f2356acaa65193b629379bc919
Sohaib SEGUENI <sohaibsegunei8@gmail.com>
2024-12-08 14:48:37 +0100

    first commit

---
+++ lib/word_count.ex
@@ -0,0 +1,18 @@
+defmodule WordCount do
+  @moduledoc """
+  Documentation for `WordCount`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WordCount.hello()
+      :world
+
+  """
+  def hello do
+    IO.puts "Hello, world!"
+  end
+end
